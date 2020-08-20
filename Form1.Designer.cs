7b1afbc37a0e71e67ead8f4429afeb913e07483e
AbhPal <60566260+AbhPal@users.noreply.github.com>
2020-08-20 23:46:36 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace importDatafromXMLtoDatabaseusingWindowsForms
+{
+    partial class ifxtd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(297, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse..";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 84);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(255, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(297, 84);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // ifxtd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 119);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ifxtd";
+            this.Text = "Import Data from XML to Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml;
+
+namespace importDatafromXMLtoDatabaseusingWindowsForms
+{
+    public partial class ifxtd : Form
+    {
+        private string fileName     = "";
+        private string fileNameOnly = "";
+        //private bool accepted = false;        // Not required
+        string currentPath  = @"C:\Users\abhij\OneDrive\Mutual_Fund\Current\";
+        string archivePath  = @"C:\Users\abhij\OneDrive\Mutual_Fund\Archieve\";
+        string errorPath    = @"C:\Users\abhij\OneDrive\Mutual_Fund\Error\";
+
+
+        public ifxtd()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an XML file";
+            openFileDialog.Filter = "XML File|*.xml";
+            openFileDialog.InitialDirectory = currentPath;
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            if(dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                this.fileName = openFileDialog.FileName;
+                string messageName = "XML file " + this.fileName + " selected.";
+                MessageBox.Show(messageName);
+            }
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+
+            //MessageBox.Show(xmlDocument);
+
+
+
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["invcon"].ConnectionString;
+
+                SqlConnection sqlConnection1 = new SqlConnection(connectionString);
+                sqlConnection1.Open();
+                string query1 = "TRUNCATE TABLE Investment.dbo.temp_hdfcMF";
+                using (SqlCommand sqlCommand1 = new SqlCommand(query1, sqlConnection1))
+                {
+                    sqlCommand1.ExecuteNonQuery();
+                }
+                sqlConnection1.Close();
+
+
+
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(this.fileName);
+
+                XmlNodeList xmlNodeList = xmlDocument.SelectNodes("/root/row");
+
+                foreach (XmlNode xmlNode in xmlNodeList)
+                {
+                    string fundName = xmlNode["Fund_Name"].InnerText;
+                    string amfiCode = xmlNode["AmfiCode"].InnerText;
+                    string planName = xmlNode["Plan_Name"].InnerText;
+                    string navDate = xmlNode["NAV_Date"].InnerText;
+                    string navAmount = xmlNode["NAV_Amount"].InnerText;
+
+
+                    SqlConnection sqlConnection2 = new SqlConnection(connectionString);
+                    sqlConnection2.Open();
+                    String query2 = "INSERT INTO dbo.temp_hdfcMF(fund_Name, amfiCode, plan_Name, nav_Date, nav_Amount) VALUES ('" + fundName + "','" + amfiCode + "','" + planName + "','" + navDate + "','" + navAmount + "')";
+                    using (SqlCommand sqlCommand2 = new SqlCommand(query2, sqlConnection2))
+                    {
+                        sqlCommand2.ExecuteNonQuery();
+                    }
+                    sqlConnection2.Close();
+
+
+
+
+
+                    // Look for the name in the connectionStrings section.
+
+
+                }
+
+                string messageBox = "XML file" + this.fileName + "has been successfully imported into database";
+                MessageBox.Show(messageBox);
+
+                string currentFile = currentPath + this.fileName;
+                string archiveFile = archivePath + this.fileName;
+                //System.IO.File.Move(currentFile, archiveFile);
+
+            }
+            catch
+            {
+                //MessageBox.Show("Can't import the selected xml file.");
+                //string currentFile = currentPath + this.fileName;
+                //string errorFile = errorPath + this.fileName;
+                //System.IO.File.Move(currentFile, errorFile);
+            }
+
+
+        }
+
+        
+    }
+}
